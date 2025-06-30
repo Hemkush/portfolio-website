320643b4173f86b0238448fbc218af50eea6af3a
Hemant Kumar Kushwaha <hemkush2201@gmail.com>
2025-06-29 23:26:37 -0400

    build changes

--- src/app/home/post/[id]/page.tsx
+++ src/app/home/post/[id]/page.tsx
@@ -1,9 +1,12 @@
 import { posts } from "@/app/lib/placeholder-data";
 import Post from "@/app/ui/components/Posts/Post";
 
-export default function Page({ params }: { params: { id: string } }) {
-    const post = posts.find((post) => post.id === params.id);
-    console.log(params.id, post);
+interface PageProps {
+  params: Promise<{ id: string }>;
+}
+export default async function Page({ params }: PageProps) {
+    const resolvedParams = await params;
+    const post = posts.find((post) => post.id === resolvedParams.id);
     return (
         <div>
             <h1>Post</h1>
