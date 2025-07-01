bf0e8cb3901a0a02d0cd591d9b252552345fa3c6
Hemant Kumar Kushwaha <hemkush2201@gmail.com>
2025-07-01 19:14:13 -0400

    3rd change

--- src/app/home/blog/page.tsx
+++ src/app/home/blog/page.tsx
@@ -14,7 +14,15 @@ export default async function Page() {
       
       {session?.user && <Link href="/home/post/insert"><Button className="outline outline-1  border-purple-700 text-purple-700 hover:bg-purple-700 hover:text-white my-5 py-2 px-4 rounded">New +</Button></Link>}
       <h1>Posts</h1>
-      {posts?.map((post) => <Post key={post.id} {...post} />)}
+      {posts?.map((post) => (
+        <Post
+          key={post.id}
+          id={post.id}
+          title={post.title}
+          content={post.content}
+          date={post.date}
+        />
+      ))}
     </div>
     )
 };
