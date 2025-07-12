ac74f8af8f3f3f02a2418e749fce0de2db6b68e3
Hemant Kumar Kushwaha <hemkush2201@gmail.com>
2025-07-11 23:20:18 -0400

    Checking 1.1

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
     GEMINI_API_KEY: process.env.GEMINI_API_KEY,
   }
 };
