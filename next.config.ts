419e3bf2c08155815f0b4bf80db1600344850ee7
Hemant Kumar Kushwaha <hemkush2201@gmail.com>
2025-07-27 14:10:18 -0400

    next config fix

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,10 @@ const nextConfig: NextConfig = {
   env: {
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
     GEMINI_API_KEY: process.env.GEMINI_API_KEY,
-    POSTGRES_URL: process.env.POSTGRES_URL
+    POSTGRES_URL: process.env.POSTGRES_URL,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    AUTH_SECRET: process.env.AUTH_SECRET
   }
 };
 
